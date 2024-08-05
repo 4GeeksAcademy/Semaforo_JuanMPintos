b4e0da49f20deaf8716b6955e8698abc22e402cb
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-08-05 11:43:08 +0000

    boton corregido

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -11,7 +11,7 @@ else {setColor("red")}
 };
 //crear funcion boton morado
 function agregarMorado(){
-	//if {color === "violet" ? "violet light encencido" : "violet light"} onClick={()=>setColor("violet")}
+	
 
 }
 	return (
@@ -22,10 +22,8 @@ function agregarMorado(){
 				<button className={color === "yellow" ? "yellow light encendido" : "yellow light"} onClick={()=>setColor("yellow")}></button>
 				<button className={color === "green" ? "green light encendido" : "green light"} onClick={()=>setColor("green")}></button>
 			</div>
-			<button id="boton" className="btn btn-danger" onClick={cambiarColor}>CambiarColor</button>
+			<button id="boton" className="btn btn-danger" onClick={cambiarColor}>Cambiar de Color</button>
 		</>
-
-
 	)
 };
 
